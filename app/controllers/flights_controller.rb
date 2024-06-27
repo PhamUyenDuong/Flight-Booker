07665388268877a8507dcf2050dd84f61b2c6c9a
PhamUyenDuong <pduon3@uic.edu>
2024-06-27 16:53:57 +0700

    flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,14 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @dates = Flight.all.map { |f| f.start_datetime.to_date }.uniq
+    @flights = Flight.all
 
-    if params[:search]
+    if params[:departure_airport_id].present? && params[:arrival_airport_id].present? && params[:start_date].present?
       @flights = Flight.where(
         departure_airport_id: params[:departure_airport_id],
         arrival_airport_id: params[:arrival_airport_id],
-        start_datetime: DateTime.parse(params[:date]).all_day
+        start_datetime: DateTime.parse(params[:start_date]).all_day
       )
-      @num_tickets = params[:passengers]
-    else
-      @flights = Flight.none
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,20 @@
 # db/seeds.rb
 
-require 'faker'
-
+# Clear existing data
 Airport.destroy_all
 Flight.destroy_all
 
-airports = ["SFO", "NYC", "LAX", "ORD", "DFW"].map do |code|
-  Airport.create!(code: code)
-end
+# Create some airports
+sfo = Airport.create(code: "SFO")
+nyc = Airport.create(code: "NYC")
+lax = Airport.create(code: "LAX")
+chi = Airport.create(code: "CHI")
 
-# Create some sample flights
-airports.combination(2).each do |departure, arrival|
-  3.times do
-    Flight.create!(
-      departure_airport: departure,
-      arrival_airport: arrival,
-      start_datetime: Faker::Time.forward(days: 30),
-      duration: rand(1..6) * 60
-    )
-  end
-end
+# Create some flights
+Flight.create(departure_airport: sfo, arrival_airport: nyc, start_datetime: DateTime.now + 1.day, duration: 360)
+Flight.create(departure_airport: nyc, arrival_airport: lax, start_datetime: DateTime.now + 2.days, duration: 300)
+Flight.create(departure_airport: chi, arrival_airport: sfo, start_datetime: DateTime.now + 3.days, duration: 420)
+Flight.create(departure_airport: lax, arrival_airport: chi, start_datetime: DateTime.now + 4.days, duration: 240)
+Flight.create(departure_airport: nyc, arrival_airport: sfo, start_datetime: DateTime.now + 5.days, duration: 360)
+
+puts "Seeded #{Airport.count} airports and #{Flight.count} flights."
